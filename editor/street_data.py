e21772e50763512ec5e4cc69846c4a7aa4a9080e
fastpinguin <jokrie04@gmail.com>
2021-07-26 16:10:36 +0200

    use of tkinter vars

--- editor/street_data.py
+++ editor/street_data.py
@@ -1,12 +1,7 @@
 from __future__ import annotations
 import logging
 import math
-
-class Street:
-    def __init__(self):
-        self._points = []
-        self.point_items = []
-        self.arc = None
+from tkinter import StringVar, IntVar, DoubleVar, BooleanVar
 
 
 class Crossing:
@@ -14,19 +9,37 @@ class Crossing:
         """class for saving of crossings. connected has to be a
         list in the form of [[crossing, number_of_lanes], [...]]
         """
-        self.position = position
-        self.connected = connected
-        self.traffic_lights = traffic_lights
+        #TODO use custom list for connected
+        self._position = [IntVar(), IntVar()]
+        self._position[0].set(position[0])
+        self._position[1].set(position[1])
+        self._connected = []
+        for c, n in connected:
+            var = IntVar()
+            var.set(n)
+            self._connected.append([c, var])
+        self._traffic_lights = BooleanVar()
+        self._traffic_lights.set(traffic_lights)
+
+    @property
+    def position(self):
+        return [self._position[0].get(), self._position[1].get()]
+
+    @position.setter
+    def position(self, new: list):
+        for i, pos in enumerate(self._position):
+            pos.set(new[i])
 
     def connect(self, other: Crossing, lanes: int):
         """Connects two Crossings, but only one way. if exists, adds lanes"""
         if not self.is_connected(other):
-            self.connected.append([other, lanes])
+            l = IntVar()
+            l.set(lanes)
+            self._connected.append([other, l])
         else:
-            for i, (c, n) in enumerate(self.connected):
+            for c, n in self._connected:
                 if c == other:
-                    self.connected[i] = [c, n+lanes]
-
+                    n.set(n.get()+lanes)
 
     def connect_both_ways(self, other: Crossing, lanes):
         """Connects two Crossings in both ways. if LANES is a list,
@@ -35,11 +48,17 @@ class Crossing:
         if isinstance(lanes, list):
             if (not isinstance(lanes[0], int)) or (not isinstance(lanes[1], int)):
                 raise ValueError("Connect_both_ways: Number needs to be of the type list [int, int] or int.")
-            self.connected.append([other, lanes[0]])
-            other.connected.append([self, lanes[1]])
+            l1 = IntVar()
+            l1.set(lanes[0])
+            l2 = IntVar()
+            l2.set(lanes[1])
+            self._connected.append([other, l1])
+            other._connected.append([self, l2])
         elif isinstance(lanes, int):
-            self.connected.append([other, lanes])
-            other.connected.append([self, lanes])
+            l = IntVar()
+            l.set(lanes)
+            self._connected.append([other, l])
+            other._connected.append([self, l])
         else:
             raise ValueError("Connect_both_ways: Number needs to be of the type list [int, int] or int.")
 
@@ -47,41 +66,43 @@ class Crossing:
         """Disconnects a crossing from another one, but ONLY in that one direction,"""
         if isinstance(lanes, int):
             # disconnect lane
-            for i, (c, n) in enumerate(self.connected):
+            for c, n in self._connected:
                 if c == other:
                     # lanes cannot be negative, so min() is used
-                    self.connected[i] = [c, n-min(lanes, n)]
+                    n.set(n.get()-min(lanes, n.get()))
         else:
             raise ValueError("lanes to dissconnect needs to be a valid int")
 
     def is_connected(self, other):
         """Checks if crossing is already connected to other crossing
         Returns Bool, <number_of_lanes> (None, int)"""
-        for i, (c, n) in enumerate(self.connected):
+        for c, n in self._connected:
             if c == other:
-                return n
+                return n.get()
         return 0
-
 
 
 class StreetData:
     def __init__(self):
-        self.streets = []
+        self.crossings = []
 
     def get_nearest(self, x, y):
-        # get the nearest street waypoint
-        nearest_street = None
+        nearest_crossing = None
         min_dist_sqr = None
-        for street in self.streets:
-            logging.debug(street)
-            # reminder: the points are a flattened list
-            for i in range(0, len(street.points), 2):
-                px = street.points[i]
-                py = street.points[i+1]
-                dist_sqr = (px - x)**2 + (py - y)**2
-                if min_dist_sqr is None or dist_sqr < min_dist_sqr:
-                    nearest_street = street
-                    min_dist_sqr = dist_sqr
-            else: return (None, None)
-        else: return (None, None)
-        return math.sqrt(min_dist_sqr), nearest_street
+        for c in self.crossings:
+            dist_sqr = (c.position[0]-x)**2 + (c.position[1]-y)**2
+            if min_dist_sqr is None or dist_sqr < min_dist_sqr:
+                nearest_crossing = c
+                min_dist_sqr = dist_sqr
+        else:
+            return nearest_crossing
+
+
+if __name__ == "__main__":
+    from tkinter import Tk
+    a = Tk()
+    cr = Crossing([3,5], [], True)
+    cs = Crossing([3,5], [], True)
+    cr.connect(cs, 5)
+    cr. disconnect(cs, 4)
+    print(cr.is_connected(cs))
