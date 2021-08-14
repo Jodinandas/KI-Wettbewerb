dd409e31ff1452be46bd3ea9a67f0fb6381fe711
Ferdinand Pamberger <fiesereiswuerfel@gmail.com>
2021-08-14 21:15:47 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # KI-Wettbewerb
+
+## TODO:
+
+* Look at different crates for machine learning
+    * for genetic algorithm: [darwin-rs](https://rustrepo.com/repo/willi-kappler-darwin-rs-rust-machine-learning)
+* Decide wether to use Reinforcement Learning or Genetic Evolution
+    * arguments for RL: 
+        - more complex simulation possible
+    * arguments for GA:
+        - can be easily executed in parallel
+        - easier to implement?
--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,6 +1,5 @@
 
 
-
 fn main() {
     let json: &str = r#"{"crossings": [{"traffic_lights": false, "is_io_node": false, "connected": [[1, 15]]}, {"traffic_lights": false, "is_io_node": false, "connected": [[0, 1], [2, 1], [3, 1], [4, 1]]}, {"traffic_lights": false, "is_io_node": false, "connected": [[1, 1], [3, 1], [4, 1], [5, 1]]}, {"traffic_lights": false, "is_io_node": false, "connected": [[2, 1], [1, 1]]}, {"traffic_lights": false, "is_io_node": false, "connected": [[1, 1], [2, 1]]}, {"traffic_lights": false, "is_io_node": true, "connected": [[2, 1]]}]}"#;
     let data = simulator::street_data::StreetData::from_json(json).unwrap();
