d2fa6e22d5f000d65bc4ad046d1a8c10ef40a542
Ferdinand Pamberger <fiesereiswuerfel@gmail.com>
2021-08-26 13:24:29 +0200

    Add dynamic feature to speed up development

--- editor-rs/src/main.rs
+++ editor-rs/src/main.rs
@@ -1,3 +1,13 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin, EguiSettings};
+// use wasm_bindgen::prelude::*;
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .run();
+
 }
