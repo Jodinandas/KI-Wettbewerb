6e1442df2f8b35fb127db107f3d8b1d21e91f4d5
Ferdinand Pamberger <fiesereiswuerfel@gmail.com>
2021-08-26 13:40:28 +0200

    Add a nice black window

--- editor-rs/src/main.rs
+++ editor-rs/src/main.rs
@@ -7,6 +7,7 @@ use bevy_egui::{egui, EguiContext, EguiPlugin, EguiSettings};
 
 fn main() {
     App::build()
+        .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .run();
 
