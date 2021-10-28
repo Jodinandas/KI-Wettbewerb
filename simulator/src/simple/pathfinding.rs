8ebd404097e5a31be3c91ef65d3787c4964f8890
fastpinguin <jokrie04@gmail.com>
2021-10-28 20:43:32 +0200

    Path Aware Car

--- simulator/src/simple/pathfinding.rs
+++ simulator/src/simple/pathfinding.rs
@@ -6,12 +6,27 @@ use rand::prelude::*;
 use rand::distributions::WeightedIndex;
 use dyn_clone::DynClone;
 use pathfinding::directed::dijkstra::dijkstra;
+use std::fmt::{Debug, Formatter, Display};
 
 #[derive(Debug, Clone)]
 struct PathAwareCar {
     speed: f32,
     path: Vec<usize>,
 }
+
+#[derive(Debug, Error)]
+struct PathError {
+    msg: &'static str,
+    expected_node: usize,
+    available_nodes: Vec<usize>
+}
+
+impl Display for PathError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "PathError: {}", self.msg)
+    }
+}
+
 
 impl Movable for PathAwareCar {
     fn get_speed(&self) -> f32 {
@@ -28,7 +43,15 @@ impl Movable for PathAwareCar {
 
     fn decide_next(&mut self, connections: &Vec<usize>) -> Result<usize, Box<dyn Error>> {
         // epische logik hier
-        Ok(1)
+        let to_return = self.path.pop()?;
+        if !connections.contains(&to_return){
+            return Err(Box::new(PathError{
+                msg: "Requested connection not present in current available in node",
+                expected_node: to_return,
+                available_nodes: connections.clone(),
+            }))
+        }
+        Ok(to_return)
     }
 }
 
@@ -43,7 +66,7 @@ impl MovableServer{
             nodes
         }
     }
-    fn generate_movable(&self, index: usize){
+    fn generate_movable(&self, index: usize) -> PathAwareCar{
         // choose random IoNode to drive to
         let io_nodes: Vec<(usize, &Box<dyn NodeBuilderTrait>)> = self.nodes.iter().enumerate().filter(
             | (i, node) | {
@@ -62,12 +85,20 @@ impl MovableServer{
         // you are the chosen one!
         let end_node_index = io_nodes[dist.sample(&mut rng)].0;
         let start_node_index = index;
-        let result = dijkstra(
+        let mut result = dijkstra(
             &start_node_index,
             |p| self.nodes[*p].get_connections().iter()
                 .map(| c_index | { (*c_index, (self.nodes[*c_index].get_weight() * 100000.0) as usize) }),
             |i| *i == end_node_index
-        );
+        ).expect("Unable to compute path").0;
+        // Reverse list of nodes to be able to pop off the last element
+        result.reverse();
+        // IONode is the first element
+        result.pop();
+        PathAwareCar{
+            speed: 1.0,
+            path: result
+        }
     }
 }
 
@@ -81,6 +112,6 @@ mod tests{
         use crate::debug::build_grid_sim;
         let simbuilder = build_grid_sim(500);
         let test = MovableServer::new(simbuilder.nodes);
-        test.generate_movable(1)
+        test.generate_movable(1);
     }
 }
