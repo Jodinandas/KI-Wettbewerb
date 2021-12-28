bc5e3a490ce2bf413c38b6b8edf739d6d0d4d757
Ferdinand Pamberger <fiesereiswuerfel@gmail.com>
2021-12-28 11:33:59 +0100

    Move Theme code to separate file

--- editor-rs/src/lib.rs
+++ editor-rs/src/lib.rs
@@ -8,6 +8,7 @@ use simulator;
 use simulator::datastructs::IntMut;
 use simulator::debug::build_grid_sim;
 use simulator::nodes::{NodeBuilder, NodeBuilderTrait};
+use themes::*;
 use toolbar::ToolType;
 use wasm_bindgen::prelude::*;
 mod user_interface;
@@ -15,6 +16,7 @@ use bevy::input::mouse::{MouseMotion, MouseWheel};
 mod sim_manager;
 mod toolbar;
 mod node_bundles;
+mod themes;
 use node_bundles::node_render;
 
 use crate::node_bundles::{CrossingBundle, IONodeBundle, StreetBundle};
@@ -92,55 +94,6 @@ impl UIState {
     }
 }
 
-/// This struct stores information about the visual style of the application
-///
-/// the colors of the simulator can be defined with fields like background
-///
-/// to change the visuals of the rest of the frontend, use the `egui_visuals` field
-#[derive(Default)]
-pub struct UITheme {
-    background: Color,
-    io_node: Color,
-    street: Color,
-    crossing: Color,
-    highlight: Color,
-    egui_visuals: Visuals,
-}
-
-#[derive(PartialEq, Clone, Copy)]
-pub enum CurrentTheme {
-    LIGHT,
-    DARK,
-}
-
-impl UITheme {
-    pub fn light() -> UITheme {
-        UITheme {
-            background: Color::rgb(255.0, 255.0, 255.0),
-            io_node: Color::rgb(0.0, 200.0, 0.0),
-            street: Color::rgb(100., 50., 0.),
-            crossing: Color::rgb(0.0, 200.0, 0.0),
-            highlight: Color::rgb(255.0, 0.0, 0.0),
-            egui_visuals: Visuals::light(),
-        }
-    }
-    pub fn dark() -> UITheme {
-        UITheme {
-            background: Color::rgb(0.0, 0.0, 0.0),
-            io_node: Color::rgb(200.0, 200.0, 0.0),
-            street: Color::rgb(255., 255., 255.),
-            crossing: Color::rgb(200.0, 200.0, 0.0),
-            highlight: Color::rgb(255.0, 0.0, 0.0),
-            egui_visuals: Visuals::dark(),
-        }
-    }
-    pub fn from_enum(theme: &CurrentTheme) -> UITheme {
-        match theme {
-            CurrentTheme::LIGHT => UITheme::light(),
-            CurrentTheme::DARK => UITheme::dark(),
-        }
-    }
-}
 
 #[derive(Debug)]
 pub enum NodeType {
