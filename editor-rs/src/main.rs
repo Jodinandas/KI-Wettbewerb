b96b18851eced5d1dd5c5e64e60ba52150c0f4c6
Ferdinand Pamberger <ferdinand.pamberger@aon.at>
2022-01-03 11:09:44 +0100

    Add note to explain logging

--- editor-rs/src/main.rs
+++ editor-rs/src/main.rs
@@ -1,9 +1,12 @@
 use editor_rs::run;
-use log::{info, error, debug};
 extern crate pretty_env_logger;
 
 pub fn main() {
+    // NOTE: The logger expects an environment variable called RUST_LOG
+    //  for logging the frontend, use RUST_LOG="editor_rs=<level>" (where <level> is either "trace", "debug", "info", "warn" or "error")
+    //      (be careful, the name editor-rs is tranformed to editor_rs)
+    //  for logging the backend, use RUST_LOG="simulator=<level>"
+    //  for both: RUST_LOG="editor_rs=<level>,simulator=<level>"
     pretty_env_logger::init();
-    debug!("running bevy");
     run()
 }
